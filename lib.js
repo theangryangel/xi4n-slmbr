e7db080c4ea260070cae5288669304eae9d1b157
Karl Southern <karl@theangryangel.co.uk>
2012-07-28 23:19:43 +0100

    Removes some coding errors, modifies the counter and backs off saving

--- lib.js
+++ lib.js
@@ -6,7 +6,7 @@ var model = {
 		var id = this.id(userid);
 		var m = database.get(id);
 		if (!m)
-			m = { distance: 0, points: 0, position: { x: 0, y: 0, z: 0 } };
+			m = { points: 0, position: { x: null, y: null, z: null }, last: (new Date().getTime()), milestone: 0 };
 
 		return m;
 	},
@@ -14,7 +14,7 @@ var model = {
 	{
 		var id = this.id(userid);
 		database.set(id, model);
-	}
+	},
 	id: function(userid)
 	{
 		return userid.toLowerCase();
@@ -29,24 +29,73 @@ exports.init = function()
 
 	this.client.on('state:connnew', function(ucid)
 	{
+		if (ucid <= 0)
+			return;
+
 		var c = this.client.state.getConnByUcid(ucid);
 
 		if (!c)
-			continue;
+			return;
 
 		var m = model.fetch(db, c.uname);
 
 		var messages = [
-			'Welcome to slmbr.',
-			'Life cruising too fast? Slmbr is for you.',
-			'Gain points by not moving.',
-			'Your current points are: ' + m.points + '.'
+			'^3Welcome to slmbr.',
+			'^3Life cruising too fast? Slmbr is for you.',
+			'^3Gain points by not moving. 1 point every 10 seconds.',
+			'^3Your current points are: ' + m.points + '.',
+			'^3Type !pts to show your current points.',
 		];
 
 		for (var i = 0; i < messages.length; i++)
 		{
 			var welcome = new this.insim.IS_MTC;
 			welcome.ucid = ucid;
+			welcome.text = messages[i];
+
+			this.client.send(welcome);
+		}
+	});
+
+	this.client.on('state:plyrnew', function(plid)
+	{
+		var p = this.client.state.getPlyrByPlid(plid);
+
+		if (!p)
+			return; // some how got a plid that we don't know about in the state
+
+		var c = this.client.state.getConnByUcid(p.ucid);
+
+		if (!c)
+			return;
+
+		var m = model.fetch(db, c.uname);
+		m.last = new Date().getTime();
+		model.save(db, c.uname, m);
+	});
+
+	this.client.on('state:plyrleave', function(plid)
+	{
+		var p = this.client.state.getPlyrByPlid(plids[i]);
+		if (!p)
+			return;
+
+		var c = this.client.state.getConnByUcid(p.ucid);
+
+		if (!c)
+			return;
+
+		var m = model.fetch(db, c.uname);
+
+		var messages = [
+			'^3Thanks for slmbr\'ing.',
+			'^3Your current points are: ' + m.points + '.'
+		];
+
+		for (var i = 0; i < messages.length; i++)
+		{
+			var welcome = new this.insim.IS_MTC;
+			welcome.ucid = p.ucid;
 			welcome.text = messages[i];
 
 			this.client.send(welcome);
@@ -61,34 +110,45 @@ exports.init = function()
 			if (!p)
 				continue;
 
-			var p = this.client.state.getPlyrByPlid(plid);
-
-			if (!p)
-				continue;
-
 			var c = this.client.state.getConnByUcid(p.ucid);
 
 			if (!c)
 				continue;
 
+			var time = new Date().getTime();
 			var m = model.fetch(db, c.uname);
 
-			if (m.position.x != 0 && m.position.y != 0 && m.position.z != 0)
+			if (m.position.x == null)
 			{
+				m.position.x = p.x;
+				m.position.y = p.y;
+				m.position.z = p.z;
+				m.last = time;
+
+				model.save(db, c.uname, m);
+			}
+
+			if ((time - m.last >= 10000) && (m.position.x != null))
+			{
+				m.last = time;
+
 				var x = m.position.x - p.x;
 				var y = m.position.y - p.y;
+				var z = m.position.z - p.z;
 				
 				var xy = x^2 + y^2;
-				var d = Math.abs(Math.sqrt(xy^2 + z^2));
+				var d = Math.abs(xy^2 + z^2);
 
-				if (distance <= 10)
-					m.points += 1;
+				if (d < 10000)
+					m.points += 1 - d;
 
-				if ((m.points % 10) == 0)
+				if ((m.points != m.milestone) && ((m.points % 10) == 0))
 				{
+					m.milestone = m.points;
+
 					var messages = [
-						'New milestone reached!',
-						'Your current points are: ' + m.points + '.'
+						'^3New milestone reached!',
+						'^3Your current points are: ' + m.points + '.'
 					];
 
 					for (var i = 0; i < messages.length; i++)
@@ -100,13 +160,36 @@ exports.init = function()
 						this.client.send(welcome);
 					}
 				}
+
+				m.position.x = p.x;
+				m.position.y = p.y;
+				m.position.z = p.z;
+
+				model.save(db, c.uname, m);
 			}
-
-			m.position.x = p.x;
-			m.position.y = p.y;
-			m.position.z = p.z;
-
-			model.save(db, c.uname, m);
 		}
+	});
+
+	this.client.on('IS_MSO', function(pkt)
+	{
+		if (pkt.usertype != this.insim.MSO_USER)
+			return;
+
+		var msg = pkt.msg.substr(pkt.msg.textstart);
+		if (msg.indexOf('!pts') < 0)
+			return;
+
+		var c = this.client.state.getConnByUcid(pkt.ucid);
+
+		if (!c)
+			return;
+
+		var m = model.fetch(db, c.uname);
+
+		var welcome = new this.insim.IS_MTC;
+		welcome.ucid = pkt.ucid;
+		welcome.text = '^3Your current points are: ' + m.points + '.';
+
+		this.client.send(welcome);
 	});
 }
